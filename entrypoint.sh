eb804458475f53b6b6814df0564b7d523f7e4152
jecem ben slama <benslemajecem@gmail.com>
2026-08-20 17:20:10 +0100

    change entrppoint.sh from CRLF to LF

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,6 @@ set -e
 envsubst '$PORT' < /etc/nginx/nginx.conf > /etc/nginx/nginx.conf.optimized
 mv /etc/nginx/nginx.conf.optimized /etc/nginx/nginx.conf
 
-# Same-origin setup: frontend nginx proxies /api, /oauth2, /login to the backend
 mkdir -p /usr/share/nginx/html/assets
 echo '{ "apiUrl": "" }' > /usr/share/nginx/html/assets/config.json
 
